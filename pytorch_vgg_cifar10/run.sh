24e4b7769515367e0663605354be29fab9c07923
Aniket Singh <aniketsinghresearch@gmail.com>
2024-02-13 08:23:06 +0000

    run

--- pytorch_vgg_cifar10/run.sh
+++ pytorch_vgg_cifar10/run.sh
@@ -2,14 +2,16 @@
 
 #for model in vgg11 vgg11_bn vgg13 vgg13_bn vgg16 vgg16_bn vgg19 vgg19_bn
 #do
-model=vgg16_bn
-echo "python main.py  --arch=$model --epochs 2 --save-dir=save_$model |& tee -a log_$modeli"
-python main.py  --arch=$model --epochs 2 --save-dir=save_$model |& tee -a log_$model
+#model=vgg16_bn
+model=vgg16
+echo "python main.py  --arch=$model --epochs 300 --save-dir=save_$model |& tee -a log_$modeli"
+python main.py  --arch=$model --epochs 300 --save-dir=save_$model |& tee -a log_$model
+
 #done
 
 # for model in vgg11 vgg11_bn vgg13 vgg13_bn vgg16 vgg16_bn vgg19 vgg19_bn
 #do
-#echo "python main.py  --arch=$model --half --save-dir=save_half_$model |& tee -a log_half_$model"
-#python main.py  --arch=$model --half --save-dir=save_half_$model |& tee -a log_half_$model
+echo "python main.py  --arch=$model --half --save-dir=save_half_$model |& tee -a log_half_$model"
+python main.py  --arch=$model --half --save-dir=save_half_$model |& tee -a log_half_$model
 #done
 
